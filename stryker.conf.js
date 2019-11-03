5953cf62beef07252776006ad4b3a0a1804f608d
Christopher J. Brody <chris@brody.consulting>
2019-11-02 21:28:46 -0400

    explicitly configure mutation to include JavaScript in bin

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,5 +1,6 @@
 module.exports = function(config) {
   config.set({
+    mutate: ["bin/**/*.js", "lib/**/*.js"],
     mutator: "javascript",
     packageManager: "yarn",
     reporters: ["html", "clear-text", "progress"],
