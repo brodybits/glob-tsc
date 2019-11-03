af68997d0da278a21b2417e4ed194129852dd214
Christopher J. Brody <chris@brody.consulting>
2019-11-02 22:54:40 -0400

    `bin/glob-tsc.js` use `execa` instead of `cross-spawn`
    remove `cross-spawn` from package dependencies
    
    skip now broken test in `test/glob-tsc.spec.js`
    
    now relying on using `checkjs` to test `bin/glob-tsc.js`

--- bin/glob-tsc.js
+++ bin/glob-tsc.js
@@ -1,25 +1,11 @@
 #! /usr/bin/env node
 
-var spawn = require('cross-spawn'),
+const execa = require('execa');
+
+var
     helper = require('../lib/program-helper'),
     options = helper.getOptions(),
     commandArgs = options.unknown.concat(helper.resolveTSFiles()),
-    proc = spawn(helper.getTSCCommand(), commandArgs, { stdio: 'inherit' });
-
-proc.on('exit', function (code, signal) {
-    process.on('exit', function(){
-        if (signal) {
-            process.kill(process.pid, signal);
-        } else {
-            process.exit(code);
-        }
-    });
-});
-
-// terminate children.
-process.on('SIGINT', function () {
-    proc.kill('SIGINT'); // calls runner.abort()
-    proc.kill('SIGTERM'); // if that didn't work, we're probably in an infinite loop, so make it die.
-});
+    proc = execa.sync(helper.getTSCCommand(), commandArgs, { stdio: 'inherit' });
 
 module.exports = process;
